45de845af03f2fffeb6fae932de188d12059c946
Alexis <xixi.gaudr@gmail.com>
2022-10-10 21:40:46 +0200

    yalescards
    Je me faisais chier du coup j'ai avancé un peu en créant vite fait une card et en modifiant quelques trucs pour que ce soit mieux même si c'est pas encore incroyable

---
+++ script.js
@@ -0,0 +1,9 @@
+function buttonProfileFunction() {
+    console.log("profile");
+};
+function buttonHomeFunction() {
+    console.log("home");
+};
+function buttonMatchFunction() {
+    console.log("match");
+};
