f8cc9eaaad3cff893273587e2bab489b4b56e671
Alexis <xixi.gaudr@gmail.com>
2022-10-11 20:19:53 +0200

    lesboutonsgrossissent
    Wouaou c'est un truc de malade quand tu cliques ça grossit comme mon zi...

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function buttonProfileFunction() {
-    console.log("profile");
-};
-function buttonHomeFunction() {
-    console.log("home");
-};
-function buttonMatchFunction() {
-    console.log("match");
+function buttonFunction(page) {
+    const buttons_ids = ["icon-profile", "icon-home", "icon-match"];
+    for (let i=0; i<3; i++) {
+        button = document.getElementById(buttons_ids[i]);
+        if (buttons_ids[i] == 'icon-' + page) {
+            button.style.scale = '200%';
+        }
+        else {
+            button.style.scale = '150%';
+        };
+    };
 };
